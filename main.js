377dba1d5c827331d7396cd759e9b7e55e3034a9
cuber-dev <118879829+cuber-dev@users.noreply.github.com>
2023-03-04 20:24:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const celsius = document.getElementById('celsius');
+const fDisplay = document.getElementById('farhenheat-display');
+const farhenheat = document.getElementById('farhenheat');
+const cDisplay = document.getElementById('celsius-display');
+
+celsius.addEventListener('input',() => {
+  if(isNaN(celsius.value)){
+    fDisplay.textContent = 'Invalid';
+  }
+  else{
+    let result = (Number(celsius.value) * 9 / 5) + 32;
+    fDisplay.textContent = result;
+  }
+});
+
+
+farhenheat.addEventListener('input', () => {
+  if (isNaN(farhenheat.value)) {
+    cDisplay.textContent = 'Invalid';
+  }
+  else {
+    let result = (Number(farhenheat.value) - 32) * 5 / 9;
+    cDisplay.textContent = result;
+  }
+});
