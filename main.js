1857de05cf0df352696533fb1d089d43965df84e
cuber-dev <118879829+cuber-dev@users.noreply.github.com>
2023-03-05 07:21:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,12 +4,20 @@ const farhenheat = document.getElementById('farhenheat');
 const cDisplay = document.getElementById('celsius-display');
 
 celsius.addEventListener('input',() => {
+  if(isNaN(celsius.value)){
+    fDisplay.textContent = "Invalid";
+  }else{
     let result = (Number(celsius.value) * 9 / 5) + 32;
     fDisplay.textContent = result;
+  }
 });
 
 
 farhenheat.addEventListener('input', () => {
+  if (isNaN(farhenheat.value)) {
+    cDisplay.textContent = "Invalid";
+  } else {
     let result = (Number(farhenheat.value) - 32) * 5 / 9;
     cDisplay.textContent = result;
+  }
 });
